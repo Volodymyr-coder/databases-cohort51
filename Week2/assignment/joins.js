2e910fc382d96551ec27fef6dd8e96059f2672d3
Volodymyr-coder <volodymyr.coder2024@gmail.com>
2025-03-19 13:15:09 +0100

    task 3.3

--- Week2/assignment/joins.js
+++ Week2/assignment/joins.js
@@ -16,6 +16,13 @@ LEFT JOIN authors mentor ON authors.mentor = mentor.author_id;`;
     const [results] = await connection.query(selectAuthorsAndMentors);
 
     console.log('Authors and their mentors:', results);
+
+    const selectAuthorsAndPaperTitle = `SELECT author.*, research_paper.paper_title 
+FROM authors author
+LEFT JOIN research_papers research_paper ON author.author_id = research_paper.paper_id`;
+
+    const [result] = await connection.query(selectAuthorsAndPaperTitle);
+    console.log('All info about authors and their paper_title:', result);
   } catch (error) {
     console.error('Error: ', error.message);
   } finally {
